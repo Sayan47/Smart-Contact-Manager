349243631895b334c2d9c9aea33f321c72337d8a
Sayan Sarkar <sayrit89ss@gmail.com>
2023-09-23 19:37:09 +0530

    Home page front end

--- Smart Contact Manager/src/main/java/com/smart/controllers/HomeController.java
+++ Smart Contact Manager/src/main/java/com/smart/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home(ModelMap map) {
 		map.addAttribute("title", "Smart Contact Manager");
 		return "home";
